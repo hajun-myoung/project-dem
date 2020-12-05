b95691e68d912a3ef54d97279904fbfeb77dd2db
unknown <audgkwns44@gmail.com>
2020-12-05 11:09:36 +0900

    almost done. Making random Num

--- dem.js
+++ dem.js
@@ -1,27 +1,45 @@
 let dem;
+// indx.html의 DOM이 완전히 로드되길 기다리는 역할. 로드되면 필요한 것들을 불러옴.
+// 만약 EventListener를 거치지 않으면? DOM이 로드되기 전에 getElement를 실행 > null 반환됨.
 document.addEventListener('DOMContentLoaded', function () {
     dem = document.getElementById('download');
     dem.onclick = download;
 })
 
-let contents = new Uint8Array;
-function random(fileSize) {
-    //파일 사이즈만큼 랜덤한 수열 생성하는 함수 짜야 함.
-}
-
+// 다운로드 버튼을 누르면 실행되는 함수.
+// original from <javascript.info>
 function download() {
+
+    function randomString(fileSize) {
+        let date = new Date();
+        let timeNum = date.getSeconds() * 100 + date.getMilliseconds();
+        // console.log(timeNum);
+
+        let randomNum = timeNum % 256;
+        // console.log(randomNum);
+        for (let idx = 0; idx < fileSize; idx++) {
+            contents.push(randomNum);
+            randomNum = randomNum * 512795 % 256;
+            while (randomNum == 0) {
+                randomNum = date.getMilliseconds();
+            }
+            // console.log(idx + ": " + randomNum);
+        }
+    }
+
+    let contents = [];
+    let fileSize = document.getElementById('fileSize').value;
+    randomString(fileSize);
+
     let link = document.createElement('a');
     link.download = document.getElementById('fileName').value;
 
-    let blob = new Blob([contents], {
+    // ↓ blob 구성하고, 다운로드시키는 코드
+    let blob = new Blob([new Uint16Array(contents)], {
         type: 'text / plain'
     });
-
-    let fileSize = document.getElementById('fileSize').value;
-    random(fileSize);
 
     link.href = URL.createObjectURL(blob);
     link.click();
     URL.revokeObjectURL(link.href);
-    //다운로드시키는 함수는 js.info 통해서 완성
 }
